98851dacc3ceaa1394c584d23e44a69be67afc7f
ingpoc <gupta.rp.gurusharan@gmail.com>
2024-09-14 23:09:20 +0530

    Dialog changes

--- tabs/stock_details_tab.py
+++ tabs/stock_details_tab.py
@@ -11,21 +11,46 @@ mongo_client = MongoClient('mongodb://localhost:27017/')
 db = mongo_client['stock_data']
 collection = db['detailed_financials']
 
-def create_info_item(label, value, is_percentage=False):
+def create_info_item(label, value, is_percentage=False, is_estimate=False, is_piotroski=False):
     color = ""
-    if is_percentage:
+    icon = ""
+    if is_percentage or is_estimate or 'growth' in label.lower() or label == "Net Profit Growth":
         try:
-            value_float = float(value.strip('%'))
+            # Remove commas and '%' sign, then convert to float
+            value_float = float(value.replace(',', '').strip('%').split(':')[-1].strip())
             if value_float > 0:
                 color = "text-success"
+                icon = "▲"
             elif value_float < 0:
                 color = "text-danger"
+                icon = "▼"
+        except ValueError:
+            pass
+    elif is_piotroski:
+        try:
+            value_float = float(value)
+            if value_float > 5:
+                color = "text-success"
+                icon = "▲"
+            elif value_float < 5:
+                color = "text-danger"
+                icon = "▼"
         except ValueError:
             pass
     
+    if is_estimate:
+        if "Beats" in value:
+            color = "text-success"
+            icon = "▲"
+        elif "Missed" in value:
+            color = "text-danger"
+            icon = "▼"
+    
+    formatted_value = f"{icon} {value}" if icon else value
+    
     return html.Div([
         html.Span(label, className="stock-details-label"),
-        html.Span(value, className=f"stock-details-value {color}")
+        html.Span(formatted_value, className=f"stock-details-value {color}")
     ], className="stock-details-item")
 
 def create_info_card(title, items, icon):
@@ -34,7 +59,13 @@ def create_info_card(title, items, icon):
             html.I(className=f"fas {icon} me-2"),
             html.Span(title, className="h6 mb-0")
         ], className="d-flex align-items-center py-2"),
-        dbc.CardBody([create_info_item(label, value, 'growth' in label.lower() or 'yield' in label.lower()) for label, value in items], className="py-2")
+        dbc.CardBody([
+            create_info_item(label, value, 
+                             is_percentage='growth' in label.lower() or 'yield' in label.lower(),
+                             is_estimate='estimates' in label.lower(),
+                             is_piotroski='piotroski score' in label.lower())
+            for label, value in items
+        ], className="py-2")
     ], className="stock-details-card h-100")
 
 def stock_details_layout(company_name, show_full_layout=True):
